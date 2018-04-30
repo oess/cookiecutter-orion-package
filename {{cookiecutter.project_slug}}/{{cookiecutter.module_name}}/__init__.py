5880a9816bbe1941569158aa02abb486fad219b8
Mark McGann <mcgann@eyesopen.com>
2018-04-30 18:05:22 -0400

    Added Hands On cubes and tests

--- {{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/__init__.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.module_name}}/__init__.py
@@ -2,6 +2,9 @@ from .maxmwcube import MaxMWCube, MixinExampleMaxMWCube
 from .mycube import MyCube
 from .mwcube import MWCube, MWPropCube, ParallelMWPropCube
 from .mwfilter import MWFilterCube, MixinExampleMWFilterCube
+from .omegacube import MyOmegaCube
+from .rocscube import MyRocsCube
+from .rotbondfilter import RotatableBondFilterCube
 from .smitomol import SmiToMolCube
 
 __version__ = "0.1.0"
